8ee4de5e14a31e2ede9c40373977802417aeb818
Jon Jagger <jon@jaggersoft.com>
2018-07-13 07:08:25 +0100

    tweak test file comments

--- start_point/HikerTest.java
+++ start_point/HikerTest.java
@@ -7,12 +7,14 @@ public class HikerTest {
 
     @Test
     public void life_the_universe_and_everything() {
+        // a simple example to get you started
         int expected = 42;
         int actual = Hiker.answer();
-
-        // JUnit assertion - the default Java assertion library - https://junit.org/junit4/
+        // JUnit assertion - the default Java assertion library
+        // https://junit.org/junit4/
         assertEquals(expected, actual);
-        // assertJ assertion - fluent assertions for Java - http://joel-costigliola.github.io/assertj/
+        // assertJ assertion - fluent assertions for Java
+        // http://joel-costigliola.github.io/assertj/
         assertThat(actual).isEqualTo(expected);
     }
 }
