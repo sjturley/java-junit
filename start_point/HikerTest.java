c6521d300585e61f760e733e130a07d9b8710c01
ehei <eric.heikkila@arborinsight.com>
2018-07-13 01:03:37 -0400

    Added comments to clarify unit test styles. Swapped in the latest version of assertj

--- start_point/HikerTest.java
+++ start_point/HikerTest.java
@@ -1,4 +1,6 @@
 import org.junit.*;
+
+import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HikerTest {
@@ -7,6 +9,10 @@ public class HikerTest {
     public void life_the_universe_and_everything() {
         int expected = 42;
         int actual = Hiker.answer();
-        assertThat(expected).isEqualTo(actual);
+
+        // JUnit assertion - the default Java assertion library - https://junit.org/junit4/
+        assertEquals(expected, actual);
+        // assertJ assertion - fluent assertions for Java - http://joel-costigliola.github.io/assertj/
+        assertThat(actual).isEqualTo(expected);
     }
 }
